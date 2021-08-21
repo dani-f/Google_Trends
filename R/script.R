cfd81504452046644cce3cd9752e5d027827e761
dani-f <da.gegen@posteo.de>
2021-08-21 17:34:35 +0200

    update script

--- R/script.R
+++ R/script.R
@@ -6,8 +6,11 @@ library(dplyr)
 library(forcats)
 library(ggplot2)
 
+# Define search keyword
+search_keyword <- "Oda"
+
 # Launch search query
-dc_trends <- gtrends(keyword = "Oda",
+dc_trends <- gtrends(keyword = search_keyword,
                      time = paste("2014-01-01", Sys.Date()))
 
 # Plot interest over time
@@ -17,16 +20,24 @@ dc_trends$interest_over_time %>%
   ggplot(aes(x = date, y = hits)) +
   geom_line() +
   scale_x_date(breaks = "26 weeks") +
-  theme(axis.text.x = element_text(angle = 75, vjust = 0.58))
+  theme(axis.text.x = element_text(angle = 75, vjust = 0.58)) +
+  labs(title = "Interest over time",
+       subtitle = paste0("search keyword: '", search_keyword, "'"),
+       caption = paste0("Google Trends Query Results, ", Sys.Date()))
+ggsave("Interest_over_time.png")
 
 # Plot interest by country
 dc_trends$interest_by_country %>%
   mutate(hits = as.numeric(hits)) %>%
   filter(!is.na(hits)) %>%
-  mutate(location_2 = fct_lump_min(location, 10, w = hits),
-         location_2 = fct_reorder(location_2, hits)) %>% 
-  filter(location_2 != "Other") %>% 
-  ggplot(aes(x = location_2, y = hits)) +
+  mutate(country = fct_lump_min(location, 10, w = hits),
+         country = fct_reorder(country, hits)) %>% 
+  filter(country != "Other") %>% 
+  ggplot(aes(x = country, y = hits)) +
   geom_col() +
-  coord_flip()
+  coord_flip() +
+  labs(title = "Interest by country",
+       subtitle = paste0("search keyword: '", search_keyword, "'"),
+       caption = paste0("Google Trends Query Results, ", Sys.Date()))
 
+ggsave("Interest_by_country.png")
