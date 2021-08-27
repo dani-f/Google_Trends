421f37d14543b56b20fe498672abb71959bc3174
dani-f <da.gegen@posteo.de>
2021-08-27 15:21:24 +0200

    update script

--- R/script.R
+++ R/script.R
@@ -7,38 +7,47 @@ library(forcats)
 library(ggplot2)
 
 # Define search keyword
-search_keyword <- "Oda"
+# Comparing supermarkets
+keyword_1 <- "Oda" # Norwegian grocery store start-up
+keyword_2 <- "Lidl" # Traditional supermarket founded in Germany, now operating worldwide
+keyword_3 <- "Glovo" # Spanish quick-commerce start-up
 
 # Launch search query
-dc_trends <- gtrends(keyword = search_keyword,
+trends <- gtrends(keyword = c(keyword_1,keyword_2, keyword_3),
                      time = paste("2014-01-01", Sys.Date()))
 
+
 # Plot interest over time
-dc_trends$interest_over_time %>%
+trends$interest_over_time %>%
   mutate(date = as.Date(date),
          hits = as.numeric(ifelse(hits == "<1", 0, hits))) %>%  # hit count on 2014-03-01 is <1, which might be 0...
-  ggplot(aes(x = date, y = hits)) +
-  geom_line() +
+  ggplot(aes(x = date, y = hits, color = keyword)) +
+  geom_line(size = 1) +
+  scale_y_log10() +
   scale_x_date(breaks = "26 weeks") +
   theme(axis.text.x = element_text(angle = 75, vjust = 0.58)) +
   labs(title = "Interest over time",
-       subtitle = paste0("search keyword: '", search_keyword, "'"),
-       caption = paste0("Google Trends query results, ", Sys.Date()))
+       caption = paste0("Google Trends query results, ", Sys.Date()),
+       color = 'Search Keywords')
 
-ggsave(filename = "Output/1_interest_over_time.png")
+ggsave(filename = "Output/1_interest_over_time.jpg")
 
 # Plot interest by country
-dc_trends$interest_by_country %>%
+trends$interest_by_country %>%
   mutate(hits = as.numeric(hits)) %>%
   filter(!is.na(hits)) %>%
-  mutate(country = fct_lump_min(location, 10, w = hits),
-         country = fct_reorder(country, hits)) %>% 
+  group_by(keyword) %>%
+  mutate(country = fct_lump(f = location, n = 5, w = hits)) %>%
+  ungroup() %>% 
   filter(country != "Other") %>% 
-  ggplot(aes(x = country, y = hits)) +
+  ggplot(aes(x = country, y = hits, fill = keyword)) +
   geom_col() +
   coord_flip() +
+  facet_grid(keyword ~ ., scales = "free") +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust=1)) +
   labs(title = "Interest by country",
-       subtitle = paste0("search keyword: '", search_keyword, "'"),
-       caption = paste0("Google Trends query results, ", Sys.Date()))
+       subtitle = "Top n countries per keyword",
+       caption = paste0("Google Trends query results, ", Sys.Date())) +
+  guides(fill = FALSE)
 
-ggsave("Output/2_interest_by_country.png")
+ggsave("Output/2_interest_by_country.jpg")
